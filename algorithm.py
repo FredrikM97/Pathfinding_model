2513c3c966b8aff6f8221132e9d3575964acea9c
thunderoux <fredrik9779@gmail.com>
2020-01-18 19:51:07 +0100

    Started to implement queue and Astar

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,6 @@
+import heapq
+from random import randint
+
 class Astar:
     def __init__(self):
         pass
@@ -5,3 +8,44 @@ class Astar:
 class greedy:
     def __init__(self):
         pass
+
+
+class Struct:
+    def __init__(self):
+        self.parent = [] # No parent
+        self.node = {}
+
+    def addNode(self,parent, node, cost, g):
+        if not node in self.node:
+            self.createNode(node, parent, cost, g)
+
+    def getNode(self, node):
+        if not node in self.node:
+            return self.createNode(node)
+        else:
+            return self.node[node]
+    
+    def exists(self, node):
+        return node in self.node
+
+    def createNode(self,node, parent=0, cost=0, g=0):
+        self.node[node] = {
+                'parent':parent,
+                'cost':cost,
+                'g':g
+            }
+        return self.node[node]
+
+# The biggest value first therefor priority is negative
+class Queue:
+    def __init__(self):
+        self.elements = []
+    def isEmpty(self):
+        return len(self.elements) == 0
+    def add(self, item, priority):
+        heapq.heappush(self.elements,(priority,item))
+
+    def remove(self):
+        return heapq.heappop(self.elements)[1]
+    def printArray(self):
+        return self.elements
